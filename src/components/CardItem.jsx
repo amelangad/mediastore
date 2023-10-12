6a7d78a261b9e3d7a569fc675ed7fecc343bc607
amelangad <slodzinska.magdalena@gmail.com>
2023-10-12 09:33:31 +0200

    add currency

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -7,7 +7,7 @@ export default function CardItem({id, quantity}) {
 
 const { removeItem } = useCard();
 function priceItems (id) {
-return  quantity * item.price}
+return (quantity * item.price)}
 
 
 const item = storeItems.find(item => item.id === id)
@@ -17,9 +17,9 @@ const item = storeItems.find(item => item.id === id)
     <Stack direction="horizontal" gap ={3} className ="d-flex align-items-center">
       <img src={item.imageUrl} style={{objectFit:"scale-down", width: "120px", height: "80px"}}></img>
       <p>{item.name}</p>
-      <p className ="light">{item.price}</p>
+      <p className ="light">{item.price} zł</p>
       <p>x{quantity}</p>
-      <p className ="font-weight-bold">{priceItems(id)}</p>
+      <p className ="font-weight-bold font-xl">{priceItems(id)} zł</p>
       <Button 
       variant ="light"
       className ="d-flex justify-content-center" 
--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -11,7 +11,7 @@ export default  function Store() {
   return (
 <div className="w-100 position-relative" style ={{background: "black"}}>
     <Navbar/>
-    <Button variant= "light" onClick={handleShow} style={{width:"40px", height: "40px", borderRadius: "20%", zIndex:"50", position:"absolute", top:"10px", right: "100px" }}>
+    <Button variant= "light" onClick={handleShow} style={{width:"40px", height: "40px", borderRadius: "20%", zIndex:"50", position:"absolute", top:"20px", right: "30px"}}>
        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-cart4" viewBox="0 0 16 16">
   <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
 </svg>
@@ -21,7 +21,7 @@ export default  function Store() {
     <h1 className ="my-3 d-flex justify-content-center">Store</h1>
     <Row md={2} xs ={1} lg={3} className ="g-3" >
     {storeItems.map(item => (
-<Col key ={item.id}><StoreItem {...item}/></Col>
+<Col style={{marginBottom: "50px"}} key ={item.id}><StoreItem {...item}/></Col>
      ))}
     </Row>
     </div>
